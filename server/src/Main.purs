24d633aeab7ca10fbf3ab6fbf2f248fda372347f
Phil Freeman <paf31@cantab.net>
2015-04-24 11:23:37 -0700

    More work on service methods

--- server/src/Main.purs
+++ server/src/Main.purs
@@ -5,6 +5,8 @@ import Debug.Trace
 import Data.Int
 import Data.Maybe
 import Data.Tuple
+import Data.Array (filter, map, nub, sort)
+import Data.Foldable (elem)
 
 import qualified Data.Map as M
 
@@ -26,15 +28,19 @@ newtype Lang = Lang
   , rating :: Int
   , tags :: [String]
   }
+  
+runLang (Lang lang) = lang
 
 type DB = M.Map Key Lang
+
+-- Ratings based on number of GitHub search results
 
 purescript :: Lang
 purescript = Lang
   { name: "PureScript"
   , description: "A small strongly typed programming language that compiles to JavaScript"
   , homepage: "http://purescript.org/"
-  , rating: zero
+  , rating: fromNumber 463
   , tags: [ "Pure", "Functional", "Static", "AltJS" ]
   }
   
@@ -43,7 +49,7 @@ haskell = Lang
   { name: "Haskell"
   , description: "An advanced purely-functional programming language"
   , homepage: "http://haskell.org/"
-  , rating: zero
+  , rating: fromNumber 16586
   , tags: [ "Pure", "Functional", "Static", "Lazy" ]
   }
 
@@ -52,7 +58,7 @@ clojure = Lang
   { name: "Clojure"
   , description: "A dynamic programming language that targets the Java Virtual Machine"
   , homepage: "http://clojure.org/"
-  , rating: zero
+  , rating: fromNumber 14616
   , tags: [ "Functional", "JVM", "Dynamic", "Lisp" ]
   }
 
@@ -61,7 +67,7 @@ befunge = Lang
   { name: "Befunge"
   , description: "A two-dimensional esoteric programming language"
   , homepage: "https://esolangs.org/wiki/Befunge"
-  , rating: zero
+  , rating: fromNumber 153
   , tags: [ "2D", "Esoteric" ]
   }
 
@@ -76,13 +82,23 @@ initialDb = M.fromList
 indexHandler :: Handler
 indexHandler = send 
   { name: "langdb server"
-  , follow: "http://localhost:9000/lang"
+  , lang: "http://localhost:9000/lang"
+  , tag: "http://localhost:9000/tag"
+  }
+ 
+shortLang :: Tuple Key Lang -> { key :: Key, name :: String, uri :: String, like :: String, dislike :: String }
+shortLang (Tuple key (Lang o)) = 
+  { key: key
+  , name: o.name
+  , uri: "http://localhost:9000/lang/" <> key 
+  , like: "http://localhost:9000/lang/" <> key <> "/like"
+  , dislike: "http://localhost:9000/lang/" <> key <> "/dislike"
   }
 
 listHandler :: RefVal DB -> Handler
 listHandler db = do
   m <- liftEff $ readRef db
-  send $ fromTuple <$> M.toList m
+  send <<< map shortLang <<< M.toList $ m
   where
   fromTuple (Tuple key (Lang o)) = { key: key, name: o.name, uri: "http://localhost:9000/lang/" <> key }
 
@@ -98,13 +114,88 @@ getHandler db = do
           setStatus 404
           send "Not found"
         Just val -> send val
+        
+rateHandler :: RefVal DB -> (Int -> Int) -> Handler
+rateHandler db f = do
+  idParam <- getRouteParam "id"
+  case idParam of
+    Nothing -> nextThrow (error "id parameter is required") 
+    Just _id -> do
+      m <- liftEff $ readRef db
+      case M.lookup _id m of
+        Nothing -> do
+          setStatus 404
+          send "Not found"
+        Just _ -> do
+          liftEff $ modifyRef db $ M.update (\(Lang lang) -> Just $ Lang (lang { rating = f lang.rating })) _id
+          send "ok"
+        
+putHandler :: RefVal DB -> Handler
+putHandler db = do
+  idParam <- getRouteParam "id"
+  case idParam of
+    Nothing -> nextThrow (error "id parameter is required") 
+    Just _id -> do
+      name <- getBodyParam "name"
+      description <- getBodyParam "description"
+      homepage <- getBodyParam "homepage"
+      tags <- getBodyParam "tags"
+      let lang = { name: _
+                 , description: _
+                 , homepage: _
+                 , rating: zero :: Int
+                 , tags: _
+                 } <$> name
+                   <*> description
+                   <*> homepage
+                   <*> tags
+      case lang of
+        Nothing -> do
+          setStatus 406
+          send "Parameters missing"
+        Just lang -> do
+          liftEff $ modifyRef db $ M.insert _id $ Lang lang
+          send "ok"
+        
+tagsHandler :: RefVal DB -> Handler
+tagsHandler db = do
+  m <- liftEff $ readRef db
+  send <<< map makeEntry <<< sort <<< nub $ do
+    lang <- M.values m
+    (runLang lang).tags
+  where
+  makeEntry tag = { tag: tag, uri: "http://localhost:9000/tag/" <> tag }
+  
+tagHandler :: RefVal DB -> Handler
+tagHandler db = do
+  tagParam <- getRouteParam "tag"
+  case tagParam of
+    Nothing -> nextThrow (error "tag parameter is required") 
+    Just _tag -> do
+      m <- liftEff $ readRef db
+      send <<< map shortLang <<< filter (hasTag _tag) <<< M.toList $ m
+  where
+  hasTag t (Tuple _ (Lang o)) = t `elem` o.tags 
+
+foreign import jsonBodyParser "var jsonBodyParser = require('body-parser').json()" :: forall middleware. middleware
 
 app :: RefVal DB -> App
 app db = do
   setProp "json spaces" 2
+  
+  useExternal jsonBodyParser
+  
   get "/" indexHandler
-  get "/lang/" (listHandler db)
+  
+  get "/lang" (listHandler db)
   get "/lang/:id" (getHandler db)
+  put "/lang/:id" (putHandler db)
+
+  post "/lang/:id/like" (rateHandler db (+ one))
+  post "/lang/:id/dislike" (rateHandler db (+ (fromNumber (-1))))
+  
+  get "/tag" (tagsHandler db)
+  get "/tag/:tag" (tagHandler db)
 
 main = do
   db <- newRef initialDb
